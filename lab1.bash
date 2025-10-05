b126f90a05f206082cfe9545d354f0373cd3fe5e
I <187439614+soloflare1@users.noreply.github.com>
2025-10-04 20:49:22 -0700

    Add initial Bash script for OS lab demonstration
    This script demonstrates basic Bash commands including variable assignment, user input, and output.

---
+++ lab1.bash
@@ -0,0 +1,28 @@
+#                             Online Bash Shell.
+#                 Code, Compile, Run and Debug Bash script online.
+# Write your code in this editor and press "Run" button to execute it.
+
+
+echo "Hello World, I'm Nosratee Jahan Naba";
+echo it is OS lab;
+
+# space sensative, if we write id = 5,then it will through error
+id=5
+echo $id
+
+# print multiple items,use space
+
+Course_Name="OS";
+Course_ID=311;
+echo $Course_Name $Course_ID;
+
+
+# User input, here cin is read
+echo enter any number:
+read num
+echo $num
+
+# Taking multiple input
+echo enter your name and password
+read user_name user_pass
+echo $user_name $user_pass
