f43f2b1a225b211011e7d7f0e2e7acae776b2c27
BeMe <187439614+soloflare1@users.noreply.github.com>
2025-10-12 03:52:39 +0100

    Update Conditional_operator.bash

--- lab 1/Conditional_operator.bash
+++ lab 1/Conditional_operator.bash
@@ -1,13 +1,17 @@
 # Conditional operator
-a=20
-b=12
+
+# a=20
+# b=12
+
+read a b
 
 if [ $a -gt $b ]
 then
-	echo a is grater than b
+    echo "a is greater than b"
 elif [ $a -lt $b ]
 then
-	echo a is less than
+    echo "a is less than b"
 else
-    echo a and b are equal
+    echo "a and b are equal"
 fi
+
