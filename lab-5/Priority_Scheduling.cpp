7bf656d852a351b2fdb520761dca3c5f0754ab2a
soloflare <187439614+soloflare1@users.noreply.github.com>
2025-12-14 09:18:36 +0600

    Update Priority_Scheduling.cpp

--- lab-5/Priority_Scheduling.cpp
+++ lab-5/Priority_Scheduling.cpp
@@ -57,3 +57,115 @@ int main()
 
     return 0;
 }
+
+//    Input
+// Enter Number of Processes: 3
+// Enter Burst time & Priority for Process 1: 10 3
+// Enter Burst time & Priority for Process 2: 1 1
+// Enter Burst time & Priority for Process 3: 2 2
+
+//     Output
+// Process	Priority	Burst Time	Waiting Time	Turnaround Time
+// P2	    1		    1		    0		    1
+// P3	    2		    2		    1		    3
+// P1	    3		    10		    3		    13
+
+// Average Waiting Time: 1.33
+// Average Turnaround Time: 5.67
+
+
+
+
+
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter Number of Processes: ";
+    cin >> n;
+
+    int bt[n], wt[n], tat[n], priority[n], process[n];
+
+    // Input burst time and priority
+    for (int i = 0; i < n; i++)
+    {
+        process[i] = i + 1;
+        cout << "Enter Burst time & Priority for Process " << i + 1 << ": ";
+        cin >> bt[i] >> priority[i];
+    }
+
+    // Sort according to priority (lower value = higher priority)
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (priority[i] > priority[j])
+            {
+                swap(priority[i], priority[j]);
+                swap(bt[i], bt[j]);
+                swap(process[i], process[j]);
+            }
+        }
+    }
+
+    // Waiting Time
+    wt[0] = 0;
+    for (int i = 1; i < n; i++)
+    {
+        wt[i] = wt[i - 1] + bt[i - 1];
+    }
+
+    // Turnaround Time
+    for (int i = 0; i < n; i++)
+    {
+        tat[i] = wt[i] + bt[i];
+    }
+
+    // Output
+    cout << "\nProcess\tPriority\tBurst Time\tWaiting Time\tTurnaround Time\n";
+    float avg_wt = 0, avg_tat = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "P" << process[i] << "\t"
+             << priority[i] << "\t\t"
+             << bt[i] << "\t\t"
+             << wt[i] << "\t\t"
+             << tat[i] << endl;
+
+        avg_wt += wt[i];
+        avg_tat += tat[i];
+    }
+
+    cout << fixed << setprecision(2);
+    cout << "\nAverage Waiting Time: " << avg_wt / n << endl;
+    cout << "Average Turnaround Time: " << avg_tat / n << endl;
+
+    return 0;
+}
+
+/*
+Input:
+Enter Number of Processes: 3
+Enter Burst time & Priority for Process 1: 10 3
+Enter Burst time & Priority for Process 2: 1 1
+Enter Burst time & Priority for Process 3: 2 2
+
+Output:
+Process	Priority	Burst Time	Waiting Time	Turnaround Time
+P2	    1		    1		    0		    1
+P3	    2		    2		    1		    3
+P1	    3		    10		    3		    13
+
+Average Waiting Time: 1.33
+Average Turnaround Time: 5.67
+*/
+
